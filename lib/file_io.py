61a0ee3376a4595df1118c9838895f6a86786ef6
Hanson <hanson.hua903@gmail.com>
2023-11-10 03:36:12 -0600

    Complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt","w") as outfile:
+        outfile.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as infile:
+        infile.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as infile:
+        return infile.read()
